30566a7f6d3b9560459a75ede384c6d55084fae7
louis baptiste <louisvis@ymail.com>
2020-03-24 00:14:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -48,3 +48,7 @@ constructor (name, homeState, training){
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   }
 }
+
+class Ceo extends BoardMember{
+
+}
