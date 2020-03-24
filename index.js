45d8bf07fc1b2b4ab98949f60cc2dc8c2a480862
louis baptiste <louisvis@ymail.com>
2020-03-24 00:13:24 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,50 @@
+// function BoardMember(name, homeState, training){
+//   this.name = name
+//   this.homeState = homeState
+//   this.training = training
+//
+//   this.veto = function(){
+//     return 'No, I must disagree'
+//   }
+//   this.approve = function(){
+//     return 'You can do that!'
+//   }
+//   this.doCharity = function(){
+//     return "I like to help people."
+//   }
+//
+//   this.releasePressStatement = function(){
+//     return "You will see great things from Scuber."
+//   }
+//
+//   this.sayHi = function(){
+//     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+//   }
+// }
+
+class BoardMember {
+constructor (name, homeState, training){
+  this.name = name
+  this.homeState = homeState
+  this.training = training
+}
+
+
+  veto(){
+    return 'No, I must disagree'
+  }
+ approve(){
+    return 'You can do that!'
+  }
+ doCharity(){
+    return "I like to help people."
+  }
+
+  releasePressStatement(){
+    return "You will see great things from Scuber."
+  }
+
+  sayHi(){
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+  }
+}
